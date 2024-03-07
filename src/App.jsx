54bd9b2f3e014965a9e70997bc6ee480d3847b94
Wessley Silva <wessley.silva@outlook.com.br>
2024-03-07 09:00:57 -0300

    adding: favicon, logo icon, able set task w/ 'Enter' key and disable empty tasks

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,19 @@ const data = []
 export function App() {
   const inputRef = useRef(null)
   const [tasks, setTasks] = useState(data)
+  
+  function handleAddTask() {
+    if (inputRef.current.value){
+      const newTask = {
+        id: tasks.length + 1,
+        title: inputRef.current.value,
+        isCompleted: false
+      }
 
-function handleAddTask() {
-  const newTask = {
-    id: tasks.length + 1,
-    title: inputRef.current.value,
-    isCompleted: false
+      setTasks([...tasks, newTask])
+      inputRef.current.value = ""
+      } 
   }
-
-  setTasks([...tasks, newTask])
-  inputRef.current.value = ""
-}
 
 function handleCompleteTask(id) {
   const taskIndex = tasks.findIndex(item => item.id == id);
@@ -34,18 +36,22 @@ function handleCompleteTask(id) {
 
   return (
   <main className={styles.container}>
-    <h1 className={styles.title}>To Do App</h1>
+    <div className="logo_group">
+      <img src="../public/list-solid.svg" alt="icone lista" className='logo_img' />
+      <h1 className={styles.title}>To Do App</h1>
+    </div>
     <div className={styles.inputGroup}>
-      <input className={styles.input} ref={inputRef} placeholder='Ex: English Class' type="text"/>
+      <input className={styles.input} ref={inputRef} placeholder='Ex: English Class' type="text" onKeyDown={(event) => event.key == "Enter" && handleAddTask()}/>
       <button className={styles.button} onClick={handleAddTask}>Add</button>
     </div>
-
+      
     <div className={styles.tasks}>
       {tasks.length > 0 && tasks.map(item => (
       <Task key={item.id} task={item} handleCompleteTask={handleCompleteTask} />
       ))}
 
-      {!tasks.length && <p>Nada para fazer, descanse! &#128513;</p>}
+
+      {!tasks.length && <p>Nothing here, take a rest or add a new task! &#128513;</p>}
 
     </div>
 
